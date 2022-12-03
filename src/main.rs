af0768651af3a7968a2c152e87ce76fc64397ab9
CatHandRuler <CatHandRuler@users.noreply.github.com>
2022-12-03 19:54:37 +0900

    Add code to log in

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,29 @@
+use serenity::{
+    async_trait,
+    model::prelude::Ready,
+    prelude::{Context, EventHandler, GatewayIntents},
+    Client,
+};
 use std::env;
-
-use serenity::async_trait;
-use serenity::prelude::*;
-use serenity::model::channel::Message;
-use serenity::framework::standard::macros::{command, group};
-use serenity::framework::standard::{StandardFramework, CommandResult};
-
-#[group]
-#[commands(ping)]
-struct General;
 
 struct Handler;
 
 #[async_trait]
-impl EventHandler for Handler {}
-
-#[tokio::main]
-async fn main() {
-    let framework = StandardFramework::new()
-        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
-        .group(&GENERAL_GROUP);
-
-    // Login with a bot token from the environment
-    let token = env::var("DISCORD_TOKEN").expect("token");
-    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
-    let mut client = Client::builder(token, intents)
-        .event_handler(Handler)
-        .framework(framework)
-        .await
-        .expect("Error creating client");
-
-    // start listening for events by starting a single shard
-    if let Err(why) = client.start().await {
-        println!("An error occurred while running the client: {:?}", why);
+impl EventHandler for Handler {
+    async fn ready(&self, _ctx: Context, _status: Ready) {
+        println!("Ready");
     }
 }
 
-#[command]
-async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Pong!").await?;
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("Expected token; No token");
+    let mut client = Client::builder(&token, GatewayIntents::empty())
+        .event_handler(Handler)
+        .await
+        .expect("Err while creating Client");
 
-    Ok(())
+    if let Err(why) = client.start().await {
+        println!("Err with client: {:?}", why);
+    }
 }
